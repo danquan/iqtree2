b9f059219ab558e88d8b9e0993493400a3c3bfb6
SliferSkyd <22021108@vnu.edu.vn>
2024-06-16 06:58:54 +0700

    update

--- tree/phylosupertree.cpp
+++ tree/phylosupertree.cpp
@@ -739,8 +739,13 @@ void PhyloSuperTree::computeProcPartitionOrder(double *cost)
 #endif
 
 
-void PhyloSuperTree::reComputeProcPartitionOrder(double *cost)
+void PhyloSuperTree::reComputeProcPartitionOrder(double *_cost)
 {
+	double *cost = new double[size()];
+	for (int i = 0; i < size(); i++)
+	{
+		cost[i] = _cost[i];
+	}
 	int ntrees = size();
 	int nprocs = MPIHelper::getInstance().getNumProcesses();
 	int *id = new int[ntrees];
@@ -771,10 +776,6 @@ void PhyloSuperTree::reComputeProcPartitionOrder(double *cost)
 
 			pq.push(make_pair(proc_cost, proc_id));
 		}
-	}
-	for (int i = 0; i < ntrees; i++)
-	{
-		cost[i] = - cost[i];
 	}
 	proc_part_order = MPIHelper::getInstance().getProcVector(proc_parts);
 }
